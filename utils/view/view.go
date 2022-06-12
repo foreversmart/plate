7c9d33a2407c2cf730a3db7074748d11cbcc9661
rorschach <njutree@gmail.com>
2022-06-12 23:48:45 +0800

    add view parse draft

--- utils/view/view.go
+++ utils/view/view.go
@@ -1,27 +1,80 @@
 package view
 
 import (
-	"github.com/valyala/fastjson"
+	"fmt"
 	"reflect"
 )
 
 type View struct {
-	data map[string]*fastjson.Value
+	Fields map[string]*Field
+}
+
+type demo1 struct {
+	a    string "a"
+	nest struct {
+		p int `json:"p"`
+	} `json:"nest"`
+}
+
+type demo2 struct {
+	a    string "a"
+	p    string `json:"p"`
+	nest struct {
+		p int `json:"p"`
+	} `json:"nest"`
 }
 
 type Field struct {
-	tp      reflect.Type
-	value   reflect.Value
-	tag     string
-	isBasic bool
-	// map, array, nested struct
-	Nest  map[string]*Field
-	Array []*Field
-	Map   map[string]*Field
+	tp       reflect.Type
+	value    reflect.Value
+	tag      string
+	isStruct bool
+	// only support struct nested
+	ChildView *View
 }
 
-func FetchViewFrom(tag string, v interface{}) *View {
-	return nil
+func fetchViewFrom(tag string, v reflect.Value) (view *View, err error) {
+	for v.Kind() == reflect.Ptr {
+		if v.IsNil() {
+			return nil, fmt.Errorf("cant fetch view from a nil object %s", v.Type().String())
+		}
+		v = v.Elem()
+	}
+
+	if v.Kind() != reflect.Struct {
+		return nil, fmt.Errorf("cant fetch view from type %s", v.Type().String())
+	}
+
+	view = &View{
+		Fields: make(map[string]*Field),
+	}
+
+	fieldNum := v.Type().NumField()
+	for i := 0; i < fieldNum; i++ {
+		field := v.Type().Field(i)
+		fieldTag := field.Tag.Get(tag)
+		if fieldTag == "" || fieldTag == "-" {
+			continue
+		}
+
+		vf := &Field{
+			tp:    field.Type,
+			value: v.Field(i),
+			tag:   field.Tag.Get(tag),
+		}
+
+		if field.Type.Kind() == reflect.Struct {
+			vf.isStruct = true
+			childView, err := fetchViewFrom(tag, vf.value)
+			if err != nil {
+				return nil, err
+			}
+			vf.ChildView = childView
+		}
+
+	}
+
+	return
 }
 
 func (view *View) Parse(v interface{}) {
