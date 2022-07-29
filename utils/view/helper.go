92051202d9726f9b790156b033e4a47b231641f0
rorschach <njutree@gmail.com>
2022-07-29 23:04:54 +0800

    add view helper

---
+++ utils/view/helper.go
@@ -0,0 +1,65 @@
+package view
+
+import (
+	"reflect"
+)
+
+func SetValue(obV, dataV reflect.Value) (err error) {
+	for obV.Kind() == reflect.Ptr {
+		if obV.IsNil() {
+			// TODO optimize
+			ot := obV.Type().Elem()
+			obV.Set(reflect.New(ot))
+			obV.Elem().Set(dataV)
+			return
+		}
+
+		child := obV.Elem()
+		// break loop pointer
+		if obV == child {
+			break
+		}
+
+		obV = child
+	}
+
+	if obV.Type() == dataV.Type() {
+		obV.Set(dataV)
+	}
+
+	return nil
+}
+
+func settableValue(v reflect.Value) reflect.Value {
+	for v.Kind() == reflect.Ptr {
+		if v.IsNil() {
+			ot := v.Type().Elem()
+			v.Set(reflect.New(ot))
+		}
+
+		child := v.Elem()
+		// break loop pointer
+		if v == child {
+			break
+		}
+
+		v = child
+	}
+
+	return v
+}
+
+func toString(v reflect.Value) string {
+
+	return ""
+
+}
+
+func isSupportString(v reflect.Value) bool {
+	switch v.Kind() {
+	case reflect.String, reflect.Int, reflect.Int8:
+
+	}
+
+	return false
+}
