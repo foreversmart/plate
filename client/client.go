4e8302fc173a363c3513d8b3c588444d17d7d9e8
rorschach <njutree@gmail.com>
2022-08-05 20:22:37 +0800

    add a simple client call eg.

--- client/client.go
+++ client/client.go
@@ -9,30 +9,49 @@ import (
 	"net/http"
 )
 
-func DoRequest(logger logger.Logger, uri string, args, ret interface{}) error {
-	bt, _ := json.Marshal(args)
-	reader := bytes.NewReader(bt)
-	resp, err := http.Post(uri, "application/json", reader)
+func Call(method, url string, req, resp interface{}, header http.Header, logger logger.Logger) (err error) {
+	var request *http.Request
+
+	if req != nil {
+		bt, _ := json.Marshal(req)
+		reader := bytes.NewReader(bt)
+		request, _ = http.NewRequest(method, url, reader)
+		request.Header.Set("Content-Type", "application/json")
+	} else {
+		request, _ = http.NewRequest(method, url, nil)
+	}
+
+	if header != nil {
+		for k, v := range header {
+			request.Header[k] = v
+		}
+	}
+
+	res, err := http.DefaultClient.Do(request)
 	if err != nil {
 		logger.Error("post error", err)
 		return err
 	}
-	defer resp.Body.Close()
-	bt, err = ioutil.ReadAll(resp.Body)
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		logger.Error("read all error:", err)
 		return err
 	}
+
 	var extErr errors.Error
-	err = json.Unmarshal(bt, &extErr)
+	err = json.Unmarshal(body, &extErr)
 	if err == nil && extErr.Code != 0 {
 		logger.Error("service error:", extErr)
 		return &extErr
 	}
-	err = json.Unmarshal(bt, ret)
+
+	err = json.Unmarshal(body, &resp)
 	if err != nil {
 		logger.Error("unmarshal error:", err)
 		return err
 	}
-	return nil
+
+	return
 }
