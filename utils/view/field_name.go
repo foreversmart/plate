7b8724a350af917c24be1341151196fc3fa2ddc3
rorschach <njutree@gmail.com>
2022-08-05 20:24:25 +0800

    add set view filter by filter

--- utils/view/field_name.go
+++ utils/view/field_name.go
@@ -5,6 +5,9 @@ import (
 	"strings"
 )
 
+// fieldName get filedName from reflect.StructField default name is field t's name
+// tagOpt is where the field name fetch from the tag
+// len(tagOpt) > 1 will filter the tag option
 func fieldName(t reflect.StructField, tagOpt ...string) (res string, isValid bool) {
 	res = t.Name
 
@@ -12,6 +15,18 @@ func fieldName(t reflect.StructField, tagOpt ...string) (res string, isValid boo
 		tc := t.Tag.Get(tagOpt[0])
 		items := strings.Split(tc, TagOption)
 		res = items[0]
+
+		if len(tagOpt) > 1 {
+
+			if len(items) < 2 {
+				return "", false
+			}
+
+			// check tag option filter
+			if tagOpt[1] != items[1] {
+				return "", false
+			}
+		}
 	}
 
 	isValid = isFieldNameValid(res)
