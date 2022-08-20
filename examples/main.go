889979709513ab655f23c7f65179237c76acfd2c
rorschach <njutree@gmail.com>
2022-08-20 23:09:20 +0800

    add example log eg.

--- examples/main.go
+++ examples/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"github.com/foreversmart/plate/application"
 	"github.com/foreversmart/plate/config"
+	"github.com/foreversmart/plate/logger"
+	"github.com/foreversmart/plate/middleware"
 	"os"
 )
 
@@ -14,16 +16,25 @@ func main() {
 	}
 
 	type Req struct {
+		Logger logger.Logger `json:"logger" plate:"logger,mid"`
 	}
 
 	type Resp struct {
 		Res string `json:"res"`
 	}
+
+	app.Route().AddMiddleBefore(middleware.LogStart, &middleware.LogMidReq{})
+	app.Route().AddMiddleAfter(middleware.LogFinish, &middleware.LogFinishReq{})
+
 	app.Route().Get("ping", func(r interface{}) (resp interface{}, err error) {
+		arg := r.(*Req)
+		arg.Logger.Info("hello")
+
 		return &Resp{
 			Res: "pong",
 		}, nil
 	}, Req{})
 
 	app.Run()
+
 }
