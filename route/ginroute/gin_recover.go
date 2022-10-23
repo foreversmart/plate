52b65fca027e77ff7281c07f12b979ba865cce1c
rorschach <njutree@gmail.com>
2022-10-23 22:18:01 +0800

    remove more logs in recover

--- route/ginroute/gin_recover.go
+++ route/ginroute/gin_recover.go
@@ -6,18 +6,16 @@ import (
 	"github.com/foreversmart/plate/errors"
 	"github.com/foreversmart/plate/logger"
 	"github.com/foreversmart/plate/logger/stack"
-	"log"
 	"net/http"
 	"net/http/httputil"
-	"os"
 )
 
 func GinRecovery(recV interface{}, request *http.Request, args []interface{}) (resp interface{}, err error) {
 	stack := stack.Stack(4)
 
 	// 原生logger
-	var l *log.Logger
-	l = log.New(os.Stderr, "\n\n\x1b[31m", log.LstdFlags)
+	//var l *log.Logger
+	//l = log.New(os.Stderr, "\n\n\x1b[31m", log.LstdFlags)
 
 	httprequest, _ := httputil.DumpRequest(request, false)
 
@@ -29,7 +27,7 @@ func GinRecovery(recV interface{}, request *http.Request, args []interface{}) (r
 
 	msg := fmt.Sprintf("[Recovery] panic recovered:\n%s\n%s\n%s%s", recV, string(httprequest), stack, argStr)
 	logger.StdLog.Error(msg)
-	l.Println(msg)
+	//l.Println(msg)
 
 	return nil, errors.ErrorUnknownError
 }
